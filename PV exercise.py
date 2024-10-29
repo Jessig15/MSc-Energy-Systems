97c8547fd9a644b1befacf70414b44ca3c3fd507
Jessig15 <jessica.ibanes@lincoln.ox.ac.uk>
2024-10-29 15:28:42 +0000

    PV exercise 2
    Function

--- PV exercise.py
+++ PV exercise.py
@@ -6,23 +6,22 @@ Created on Tue Oct 29 12:31:00 2024
 """
 import math
 
-building_length = 28
-building_width = 8
-roof_angle = 22
-pv_width = 1690
-pv_height = 1046
-pv_power = 405
+def calc_pv_array_size (building_length,building_width,roof_angle,pv_width,pv_height,pv_power):
+    roof_length = building_length
+    roof_width = building_width / math.cos(math.radians(roof_angle))
 
-roof_length = building_length
-roof_width = building_width * math.cos(math.radians(roof_angle))
-
-roof_size = roof_length * roof_width
+    roof_size = roof_length * roof_width
       
-NumberOfPanels = roof_size //((pv_width/1000)*(pv_height/1000))
-print("The maximum number of PV panels is %d." %NumberOfPanels)
+    NumberOfPanels = roof_size //((pv_width/1000)*(pv_height/1000))
+    print("The maximum number of PV panels is %d." %NumberOfPanels)
 
-power_capacity = pv_power * NumberOfPanels
-print("The power capacity is %d kWp." %power_capacity)
+    power_capacity = (pv_power * NumberOfPanels)/1000
+    print("The power capacity is %d kWp." %power_capacity)
+    
+    return NumberOfPanels, power_capacity
+
+calc_pv_array_size(28,8,22,1690,1046,400)
+
 
 
 
